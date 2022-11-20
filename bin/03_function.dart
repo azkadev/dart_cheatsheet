4b44c6784a0db315202024cc5c1bf9f3b1dd0b6b
￴ <>
2022-11-20 15:35:02 +0700

    updaetg

--- bin/03_function.dart
+++ bin/03_function.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unnecessary_brace_in_string_interps, non_constant_identifier_names
+// ignore_for_file: unnecessary_brace_in_string_interps, non_constant_identifier_names, empty_catches
 
 void main(List<String> args) {
   print(calculate(59, 41));
@@ -6,6 +6,20 @@ void main(List<String> args) {
     first_name: "Gibran",
     last_name: "Alazka",
     age: 15,
+  ));
+  print(repeat("azkadev"));
+  print(repeatAdvance(
+    "azkadev",
+    isWithSpace: true,
+  ));
+  print(jsonToMessage(
+    {
+      "first_name": "azka",
+      "username": "@azkadev"
+    },
+    media: {
+      "first_name": "Nama Depan"
+    }
   ));
 }
 
@@ -28,5 +42,39 @@ String information({
     message += "\n${phone_number}";
   }
   message += "\nAge: ${age}";
+  return message;
+}
+
+String repeat(String data, [int count = 5]) {
+  return data * count;
+}
+
+String repeatAdvance(
+  String data, {
+  int count = 5,
+  bool isWithSpace = true,
+}) {
+  return ((isWithSpace) ? data += " " : data) * count;
+}
+
+String jsonToMessage(Map data, {Map media = const {}}) {
+  String message = "";
+  data.forEach((key, value) {
+    try {
+      if (value is Map || value is List) {
+        if (value is Map) {
+          message += "\n${media[key] ?? key}: ${jsonToMessage(value, media: media)}";
+        } else {
+          message += "\n${media[key] ?? key}: ${value}";
+        }
+      } else {
+        if (media.containsKey(key)) {
+          message += "\n${media[key]}: ${value}";
+        } else {
+          message += "\n${key}: ${value}";
+        }
+      }
+    } catch (e) {}
+  });
   return message;
 }
