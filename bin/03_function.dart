f3313e6715b1661076adf1478a7baa5f69df1991
￴ <>
2022-11-20 15:09:55 +0700

    update

--- bin/03_function.dart
+++ bin/03_function.dart
@@ -1,7 +1,22 @@
+// ignore_for_file: unnecessary_brace_in_string_interps
+
 void main(List<String> args) {
   print(calculate(59, 41));
 }
 
 num calculate(num number_one, num number_two) {
   return number_one + number_two;
+}
+
+String information({required String first_name, String? last_name, String? phone_number, required int age}) {
+  String message = "Information:";
+  message += "\nFull Name: ${first_name}";
+  if (last_name != null) {
+    message += " ${last_name}";
+  }
+  if (phone_number != null) {
+    message += "\n${phone_number}";
+  }
+  message += "\nAge: ${age}";
+  return message;
 }
